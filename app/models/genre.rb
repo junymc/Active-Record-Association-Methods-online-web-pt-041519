9ce40c2165f6e1bf672ae36e54f9784c49754afe
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-12 05:35:01 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -9,10 +9,13 @@ class Genre < ActiveRecord::Base
 
   def artist_count
     # return the number of artists associated with the genre
-    self.artist.count
+    self.artists.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    self.artists.collect do |artist|
+      artist.name
+    end
   end
 end
